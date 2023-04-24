820d78f304df2e3bb36cade2cb019fcb93b36a44
chaehaeun <chae_3210@naver.com>
2023-04-24 15:14:08 +0900

    [추가] 장바구니 리듀서 추가 #14

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaulteCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD": {
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.itme.amount;
+      return { items: updatedItems, totalAmount: updatedTotalAmount };
+    }
+
+    default:
+      return defaulteCartState;
+  }
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaulteCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
+    items: cartState.items,
     totalAmount: 0,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
